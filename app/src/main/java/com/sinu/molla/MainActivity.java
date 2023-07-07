29bcd129e07103d03be5439cfa2f51730863c75b
Woohyun Shin <cpu344@gmail.com>
2023-07-07 21:01:45 +0900

    Fix NPE when getCurrentFocus() returns null

--- app/src/main/java/com/sinu/molla/MainActivity.java
+++ app/src/main/java/com/sinu/molla/MainActivity.java
@@ -212,7 +212,6 @@ public class MainActivity extends AppCompatActivity {
             binding.pbrMainLoading.setVisibility(View.INVISIBLE);
             binding.rvMainFav.setVisibility(View.INVISIBLE);
             binding.tvMainFavName.setVisibility(View.INVISIBLE);
-            getCurrentFocus().clearFocus();
         }
         h.removeCallbacks(rUpdateStatus);
     }
