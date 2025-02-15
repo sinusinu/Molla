a0e7834e87642ab7ca2a97f1713206c02eaa2f48
Woohyun Shin <cpu344@gmail.com>
2025-02-16 02:00:09 +0900

    Update copyright years

--- app/src/main/java/com/sinu/molla/IconCache.java
+++ app/src/main/java/com/sinu/molla/IconCache.java
@@ -1,4 +1,4 @@
-// Copyright 2022-2023 Woohyun Shin (sinusinu)
+// Copyright 2022-2025 Woohyun Shin (sinusinu)
 // SPDX-License-Identifier: GPL-3.0-only
 
 package com.sinu.molla;
--- app/src/main/java/com/sinu/molla/OrderActivity.java
+++ app/src/main/java/com/sinu/molla/OrderActivity.java
@@ -1,3 +1,6 @@
+// Copyright 2022-2025 Woohyun Shin (sinusinu)
+// SPDX-License-Identifier: GPL-3.0-only
+
 package com.sinu.molla;
 
 import androidx.appcompat.app.AppCompatActivity;
