26c401ea43556475a74ca4257121deeb6bfb4146
Woohyun Shin <cpu344@gmail.com>
2025-10-11 22:10:37 +0900

    Add custom Application impl

---
+++ app/src/main/java/com/sinu/molla/MollaApplication.java
@@ -0,0 +1,15 @@
+package com.sinu.molla;
+
+import android.app.Application;
+
+public class MollaApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+}
