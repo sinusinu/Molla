09f7adb5c7ceff504261f9032b92ce66da2b0c31
Woohyun Shin <cpu344@gmail.com>
2026-02-10 18:36:02 +0900

    Happy new year!

--- app/src/main/java/com/sinu/molla/ManageCustomShortcutsActivity.java
+++ app/src/main/java/com/sinu/molla/ManageCustomShortcutsActivity.java
@@ -1,3 +1,6 @@
+// Copyright 2022-2026 Woohyun Shin (sinusinu)
+// SPDX-License-Identifier: GPL-3.0-only
+
 package com.sinu.molla;
 
 import android.annotation.SuppressLint;
--- app/src/main/java/com/sinu/molla/SettingsActivity.java
+++ app/src/main/java/com/sinu/molla/SettingsActivity.java
@@ -1,4 +1,4 @@
-// Copyright 2022-2025 Woohyun Shin (sinusinu)
+// Copyright 2022-2026 Woohyun Shin (sinusinu)
 // SPDX-License-Identifier: GPL-3.0-only
 
 package com.sinu.molla;
